38b50778e336757f4c5afe3b4c1d096d46ec54d4
BluePhantom <128402149+Blu3Ph4ntom@users.noreply.github.com>
2026-06-03 11:48:35 +0530

    Fix icon and shell

--- scripts/generate-icon.ps1
+++ scripts/generate-icon.ps1
@@ -21,11 +21,10 @@ New-Item -ItemType Directory -Path $frontendPublicPath -Force | Out-Null
 
 Add-Type -AssemblyName System.Drawing
 
-function Save-ResizedPng {
+function New-ResizedBitmap {
     param(
         [System.Drawing.Image]$Source,
-        [int]$Size,
-        [string]$Path
+        [int]$Size
     )
 
     $bitmap = [System.Drawing.Bitmap]::new($Size, $Size, [System.Drawing.Imaging.PixelFormat]::Format32bppArgb)
@@ -44,10 +43,83 @@ function Save-ResizedPng {
         $y = [int][Math]::Floor(($Size - $drawHeight) / 2)
         $graphics.DrawImage($Source, $x, $y, $drawWidth, $drawHeight)
 
-        $bitmap.Save($Path, [System.Drawing.Imaging.ImageFormat]::Png)
+        return $bitmap
     }
     finally {
         $graphics.Dispose()
+    }
+}
+
+function Save-ResizedPng {
+    param(
+        [System.Drawing.Image]$Source,
+        [int]$Size,
+        [string]$Path
+    )
+
+    $bitmap = New-ResizedBitmap -Source $Source -Size $Size
+    try {
+        $bitmap.Save($Path, [System.Drawing.Imaging.ImageFormat]::Png)
+    }
+    finally {
+        $bitmap.Dispose()
+    }
+}
+
+function New-IconDibBytes {
+    param(
+        [System.Drawing.Image]$Source,
+        [int]$Size
+    )
+
+    $bitmap = New-ResizedBitmap -Source $Source -Size $Size
+    $stream = [System.IO.MemoryStream]::new()
+    $writer = [System.IO.BinaryWriter]::new($stream)
+    try {
+        $maskStride = [int]([Math]::Floor(($Size + 31) / 32) * 4)
+        $pixelBytes = $Size * $Size * 4
+        $maskBytes = $maskStride * $Size
+
+        $writer.Write([uint32]40)
+        $writer.Write([int32]$Size)
+        $writer.Write([int32]($Size * 2))
+        $writer.Write([uint16]1)
+        $writer.Write([uint16]32)
+        $writer.Write([uint32]0)
+        $writer.Write([uint32]($pixelBytes + $maskBytes))
+        $writer.Write([int32]0)
+        $writer.Write([int32]0)
+        $writer.Write([uint32]0)
+        $writer.Write([uint32]0)
+
+        for ($y = $Size - 1; $y -ge 0; $y--) {
+            for ($x = 0; $x -lt $Size; $x++) {
+                $color = $bitmap.GetPixel($x, $y)
+                $writer.Write([byte]$color.B)
+                $writer.Write([byte]$color.G)
+                $writer.Write([byte]$color.R)
+                $writer.Write([byte]$color.A)
+            }
+        }
+
+        for ($y = $Size - 1; $y -ge 0; $y--) {
+            $maskRow = [byte[]]::new($maskStride)
+            for ($x = 0; $x -lt $Size; $x++) {
+                $color = $bitmap.GetPixel($x, $y)
+                if ($color.A -lt 128) {
+                    $byteIndex = [int][Math]::Floor($x / 8)
+                    $bit = [byte](0x80 -shr ($x % 8))
+                    $maskRow[$byteIndex] = [byte]($maskRow[$byteIndex] -bor $bit)
+                }
+            }
+            $writer.Write($maskRow)
+        }
+
+        return ,$stream.ToArray()
+    }
+    finally {
+        $writer.Dispose()
+        $stream.Dispose()
         $bitmap.Dispose()
     }
 }
@@ -59,55 +131,42 @@ function Write-IconFile {
     )
 
     $sizes = @(256, 128, 64, 48, 32, 16)
-    $pngEntries = @()
-    $tempFiles = @()
+    $entries = @()
+    foreach ($size in $sizes) {
+        $entries += [pscustomobject]@{
+            Size = $size
+            Bytes = [byte[]](New-IconDibBytes -Source $Source -Size $size)
+        }
+    }
+
+    $stream = [System.IO.File]::Create($Path)
+    $writer = [System.IO.BinaryWriter]::new($stream)
     try {
-        foreach ($size in $sizes) {
-            $tempPath = [System.IO.Path]::GetTempFileName()
-            $tempFiles += $tempPath
-            Save-ResizedPng -Source $Source -Size $size -Path $tempPath
-            $pngEntries += [pscustomobject]@{
-                Size = $size
-                Bytes = [System.IO.File]::ReadAllBytes($tempPath)
-            }
-        }
+        $writer.Write([uint16]0)
+        $writer.Write([uint16]1)
+        $writer.Write([uint16]$entries.Count)
 
-        $stream = [System.IO.File]::Create($Path)
-        $writer = [System.IO.BinaryWriter]::new($stream)
-        try {
-            $writer.Write([uint16]0)
+        $offset = 6 + (16 * $entries.Count)
+        foreach ($entry in $entries) {
+            $dimension = if ($entry.Size -eq 256) { 0 } else { $entry.Size }
+            $writer.Write([byte]$dimension)
+            $writer.Write([byte]$dimension)
+            $writer.Write([byte]0)
+            $writer.Write([byte]0)
             $writer.Write([uint16]1)
-            $writer.Write([uint16]$pngEntries.Count)
-
-            $offset = 6 + (16 * $pngEntries.Count)
-            foreach ($entry in $pngEntries) {
-                $dimension = if ($entry.Size -eq 256) { 0 } else { $entry.Size }
-                $writer.Write([byte]$dimension)
-                $writer.Write([byte]$dimension)
-                $writer.Write([byte]0)
-                $writer.Write([byte]0)
-                $writer.Write([uint16]1)
-                $writer.Write([uint16]32)
-                $writer.Write([uint32]$entry.Bytes.Length)
-                $writer.Write([uint32]$offset)
-                $offset += $entry.Bytes.Length
-            }
-
-            foreach ($entry in $pngEntries) {
-                $writer.Write($entry.Bytes)
-            }
+            $writer.Write([uint16]32)
+            $writer.Write([uint32]$entry.Bytes.Length)
+            $writer.Write([uint32]$offset)
+            $offset += $entry.Bytes.Length
         }
-        finally {
-            $writer.Dispose()
-            $stream.Dispose()
+
+        foreach ($entry in $entries) {
+            $writer.Write($entry.Bytes)
         }
     }
     finally {
-        foreach ($tempFile in $tempFiles) {
-            if (Test-Path -LiteralPath $tempFile) {
-                Remove-Item -LiteralPath $tempFile -Force
-            }
-        }
+        $writer.Dispose()
+        $stream.Dispose()
     }
 }
 
